f278f10a091603583f06b1653972ab3f358289f9
Murshid Bin Masud Sourov <61899629+mbmSouRov@users.noreply.github.com>
2022-11-26 22:54:29 +0600

    req 1-9 completed

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
 const port = process.env.PORT || 5000;
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASWORD}@cluster0.jc72o0s.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
+// console.log(uri);
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -27,6 +27,9 @@ async function run() {
       .db("laptopBikroy")
       .collection("laptopCollections");
     const usersCollection = client.db("laptopBikroy").collection("allUsers");
+    const productsCollection = client
+      .db("laptopBikroy")
+      .collection("allProducts");
 
     app.get("/laptops", async (req, res) => {
       const query = {};
@@ -54,6 +57,39 @@ async function run() {
         isSeller: user?.role === "Seller Account",
         isAdmin: user?.role === "Admin",
       });
+    });
+    app.post("/allProducts", async (req, res) => {
+      const product = req.body;
+      const result = productsCollection.insertOne(product);
+      res.send(result);
+    });
+    app.get("/allProducts", async (req, res) => {
+      const query = {};
+      const result = await productsCollection.find(query).toArray();
+      res.send(result);
+    });
+    app.get("/allProducts/:email", async (req, res) => {
+      const email = req.params.email;
+      console.log(email);
+      const query = { seller_email: email };
+      const result = await productsCollection.find(query).toArray();
+      res.send(result);
+    });
+    // app.delete("/allProducts/:email/:id", async (req, res) => {
+    //   const email = req.params.email;
+    //   const id = req.params.id;
+    //   console.log(email, id);
+    //   const query = { seller_email: email, _id: ObjectId(id) };
+    //   const result = await productsCollection.find(query).toArray();
+    //   res.send(result);
+    // });
+
+    app.delete("/allProducts/:email/:id", async (req, res) => {
+      const email = req.params.email;
+      const id = req.params.id;
+      const filter = { seller_email: email, _id: ObjectId(id) };
+      const result = await productsCollection.deleteOne(filter);
+      res.send(result);
     });
   } finally {
   }
